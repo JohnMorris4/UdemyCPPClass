5a75fb6a80668cec92e9637b3e654a5d8ed1b112
johnmorris4 <johnmorris@morrisje.com>
2019-08-28 05:32:10 -0400

    First Program

--- Sections/section4/FirstApp/FirstApp/main.cpp
+++ Sections/section4/FirstApp/FirstApp/main.cpp
@@ -8,8 +8,13 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int favNum;
+    std::cout << "Pease enter your favorite number between 1 and 100: ";
+    std::cin >> favNum;
+    
+    std::cout << "Pretty cool " << favNum << " is my favorite number as well" << std::endl;
+    
     return 0;
+    
 }
