a47cfc16f1977464c5eb14b0fb1af4d6f3e4dfc3
johnmorris4 <johnmorris@morrisje.com>
2019-08-28 05:19:28 -0400

    Setup initial folders

---
+++ Sections/section4/FirstApp/FirstApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstApp
+//
+//  Created by John Morris on 8/28/19.
+//  Copyright © 2019 John Morris. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
