a2979ddb27c3d9b9d289939fb3aae3c65d6d4448
johnmorris4 <johnmorris4@bellsouth.net>
2019-08-29 01:55:19 -0400

    Shortened of using the STD:: to using at top of the Using Namespace

--- Sections/section4/FirstApp/FirstApp/main.cpp
+++ Sections/section4/FirstApp/FirstApp/main.cpp
@@ -1,20 +1,13 @@
-//
-//  main.cpp
-//  FirstApp
-//
-//  Created by John Morris on 8/28/19.
-//  Copyright © 2019 John Morris. All rights reserved.
-//
-
 #include <iostream>
+using namespace std;
+
+
 
 int main() {
     int favNum;
-    std::cout << "Pease enter your favorite number between 1 and 100: ";
-    std::cin >> favNum;
-    
-    std::cout << "Pretty cool " << favNum << " is my favorite number as well" << std::endl;
-    
+    cout << "Pease enter your favorite number between 1 and 100: ";
+    cin >> favNum;
+    cout << "Pretty cool " << favNum << " is my favorite number as well" << endl;
     return 0;
     
 }
